7f1e9c944fdc6be1aaba324f1b584f49e6ada402
Hudson Cleiton <115666880+Primata83@users.noreply.github.com>
2023-10-17 16:40:30 -0300

    treinos

---
+++ exercicio03.py
@@ -0,0 +1,12 @@
+
+tentativa1 = 34
+tentativa2 = 29
+tentativa3 = 31
+tentativa = 36
+
+# trocar o valor da variavel 'tentativa' faz o teste acontecer
+
+if tentativa > 30:
+    print( "este resultado é maior")
+else:
+    print('Este resultado é menor')
---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+bene = 59
+
+if (bene >= 60):
+    print('Você tem direito aos beneficios')
+else:
+    print('você tem direita a um joia')
+
+# se estiver bene acima de 60, vc tem direito aos beneficios
