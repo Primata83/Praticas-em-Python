67e465158a4ace6ea52133486c5a0d98897cbbc0
Hudson Cleiton <115666880+Primata83@users.noreply.github.com>
2023-11-10 17:23:13 -0300

    Update pratica1.py
    hhh

--- pratica1.py
+++ pratica1.py
@@ -1,7 +1,14 @@
-isso = 'hud'
-hud = 1
 
-print(10 * (isso + ' '))
+#isso = 'hud'
+#hud = 1
+
+#print(10 * (isso + ' '))
 
 # fazer aparecer no terminal 10x hud só pode ser feito com variavel de TEXTO (string)
 # se fosse para dar resultado usando numeros, todo esse processo seria mais facil
+
+var = 'hudson'
+meio = 'cleiton'
+semi = 'de' 
+vari = 'paula'
+print (var, meio, semi, vari)
